4b6c9891609c188377d457c415dd062cc9b1e258
Jaimie Hart <hartjme@gmail.com>
2022-04-29 16:38:54 -0400

    added views

---
+++ levelupapi/views/event.py
@@ -0,0 +1,104 @@
+"""View module for handling requests about event types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Event, Gamer
+from django.core.exceptions import ValidationError
+from rest_framework.decorators import action
+
+
+class EventView(ViewSet):
+    """Level up event types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single event type
+        Returns:
+            Response -- JSON serialized event type
+        """
+        try:
+            event = Event.objects.get(pk=pk)
+            serializer = EventSerializer(event)
+            return Response(serializer.data)
+        except Event.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+    def list(self, request):
+        """Handle GET requests to get all event types
+        Returns:
+            Response -- JSON serialized list of event types
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        events = Event.objects.all()
+        event = request.query_params.get('event', None)
+        if event is not None:
+            events = events.filter(game_id=event)
+        # Set the `joined` property on every event
+        for event in events:
+            # Check to see if the gamer is in the attendees list on the event
+            event.joined = gamer in event.attendees.all()
+
+
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+        Returns
+            Response -- JSON serialized event instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        serializer = CreateEventSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(organizer=gamer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk):
+        """Handle PUT requests for an event
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        event = Event.objects.get(pk=pk)
+        serializer = CreateEventSerializer(event, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        event = Event.objects.get(pk=pk)
+        event.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(gamer)
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.remove(gamer)
+        return Response({'message': 'Gamer left'}, status=status.HTTP_204_NO_CONTENT)
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for event types
+    """
+    class Meta:
+        model = Event
+        fields = ('id', 'description', 'date', 'game', 'organizer', 'time', 'attendees', 'joined')
+        depth = 2
+
+class CreateEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'description', 'date', 'game', 'time']
---
+++ levelupapi/views/game.py
@@ -0,0 +1,83 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Game
+from levelupapi.models.game_type import GameType
+from levelupapi.models.gamer import Gamer
+from django.core.exceptions import ValidationError
+
+class GameView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+        Returns:
+            Response -- JSON serialized game type
+        """
+        game = Game.objects.get(pk=pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+
+
+    def list(self, request):
+        """Handle GET requests to get all game types
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        try:
+            games = Game.objects.all()
+            game_type = request.query_params.get('type', None)
+            if game_type is not None:
+                games = games.filter(gametype_id=game_type)
+
+            serializer = GameSerializer(games, many=True)
+            return Response(serializer.data)
+        except Game.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        """Handle POST operations
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        serializer = CreateGameSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(gamer=gamer)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        game = Game.objects.get(pk=pk)
+        serializer = CreateGameSerializer(game, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        game.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    # gamer = GamerSerializer(many=False)
+    # gametype = GameTypeSerializer(many=False)
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'maker', 'gamer', 'number_of_players', 'skill_level', 'gametype')
+        depth = 1
+
+class CreateGameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ['id', 'title', 'maker', 'number_of_players', 'skill_level', 'gametype']
