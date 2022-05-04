86a9e0e53468e4f2e3bb0686aede2ebdeba1d3a7
Jaimie Hart <hartjme@gmail.com>
2022-05-04 15:11:36 -0400

    finished ch 13

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -1,6 +1,7 @@
 """View module for handling requests about game types"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
@@ -87,7 +88,28 @@ class EventView(ViewSet):
     def destroy(self, request, pk):
         event = Event.objects.get(pk=pk)
         event.delete()
-        return Response(None, status=status.HTTP_204_NO_CONTENT)      
+        return Response(None, status=status.HTTP_204_NO_CONTENT) 
+    
+    
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+    
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.add(gamer)
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+    
+    
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(user=request.auth.user)
+        event = Event.objects.get(pk=pk)
+        event.attendees.remove(gamer)
+        return Response({'message': 'Gamer left'}, status=status.HTTP_204_NO_CONTENT)    
+        
     
 
 class EventSerializer(serializers.ModelSerializer):
